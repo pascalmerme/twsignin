788b586181024caf3f30aaf7065b5424db0034ad
Pascal <pascalmerme@no-log.org>
2015-03-08 23:54:43 +0100

    Debug

--- src/android/Twitter.java
+++ src/android/Twitter.java
@@ -11,11 +11,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+
+import android.content.Intent;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Twitter extends CordovaPlugin {
-    private static final String TAG = "HobnobGeolocation";
+    private static final String TAG = "LaFactoriaTwitter";
+
+    private TwitterLoginButton loginButton;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -26,9 +36,37 @@ public class Twitter extends CordovaPlugin {
 
             Log.d(TAG, "Start Twitter Plugin");
 
+            // Twitter start
+            Log.d(TAG, "Step 1");
+            loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+            Log.d(TAG, "Step 2");
+            loginButton.setCallback(new Callback<TwitterSession>() {
+                @Override
+                public void success(Result<TwitterSession> result) {
+                    // Do something with result, which provides a TwitterSession for making API calls
+                    Log.d(TAG, "Success");
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+                    // Do something on failure
+                    Log.d(TAG, "Error");
+                }
+            });
+            Log.d(TAG, "Step 3");
+
             return true;
         }
 
+        // Twitter end
+
         return false;
+    }
+
+    //Twitter result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 }
