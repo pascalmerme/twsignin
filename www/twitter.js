6b00ec1a3ba09e2a63ade3f07e5882492e99c010
Pascal <pascalmerme@no-log.org>
2015-03-23 14:26:06 +0100

    Testing Twitter logout

--- www/twitter.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    logout: function(success, failure, config) {
-        cordova.exec(success || function() {},
-             failure || function() {},
-             'TwitterManager',
-             'logout',
-             []);
-    }
-};
---
+++ www/twitterManager.js
@@ -0,0 +1,11 @@
+var TwitterManager = function() {
+};
+
+TwitterManager.logout = function(hide) {
+    exec(null, null, "TwitterManager", "logout", []);
+};
+
+module.exports = TwitterManager;
+
+
+
